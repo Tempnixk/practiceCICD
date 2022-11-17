e714c1d26ff0acd2d5a4308a69a0ff4b651a47e3
Hyunmin Cho <chm49@naver.com>
2022-11-17 13:30:12 +0900

    [Feat] Create a project

---
+++ practiceCICD/practiceCICD/practiceCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceCICDApp.swift
+//  practiceCICD
+//
+//  Created by Tempnixk on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct practiceCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
